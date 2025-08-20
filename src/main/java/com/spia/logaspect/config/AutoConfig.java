986ae005034e82db707ff7354dc25ab9f4ffd854
cmKang <felix6265@gmail.com>
2025-08-20 11:28:04 +0900

    commit 4

--- src/main/java/com/spia/logaspect/config/AutoConfig.java
+++ src/main/java/com/spia/logaspect/config/AutoConfig.java
@@ -1,9 +1,18 @@
 package com.spia.logaspect.config;
 
+import com.spia.logaspect.trace.LogTrace;
+import com.spia.logaspect.trace.ThreadLocalLogTrace;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.spia.logaspect")
 public class AutoConfig {
+
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
+    }
+
 }
--- src/main/java/com/spia/logaspect/trace/ThreadLocalLogTrace.java
+++ src/main/java/com/spia/logaspect/trace/ThreadLocalLogTrace.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 
-@Component
+//@Component
 @Slf4j
 public class ThreadLocalLogTrace implements LogTrace {
 
